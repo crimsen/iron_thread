e2718b56d49049d9825b7a97cfffe745784e583c
Tim Gröger <tim@groeger-clan.de>
2026-02-28 16:43:00 +0100

    update KindOfFabricSelect
    fixed filtering

--- src/components/kindOfFabric/KindOfFabricSelect.vue
+++ src/components/kindOfFabric/KindOfFabricSelect.vue
@@ -1,6 +1,17 @@
 <template>
-  <q-select v-model.number="model" :options="computedOptions" option-label="name" option-value="id" use-input
-    label="Kind of Fabric" input-debounce="0" @filter="filterFn"></q-select>
+  <q-select
+    v-model="model"
+    :options="options"
+    option-label="name"
+    option-value="id"
+    use-input
+    :readonly="readonly"
+    label="Kind of Fabric"
+    input-debounce="0"
+    map-options
+    emit-value
+    @filter="filterFn"
+  ></q-select>
 </template>
 
 <script setup lang="ts">
@@ -14,19 +25,27 @@ onBeforeMount(async () => {
 });
 
 const model = defineModel<number | null>();
+defineProps<{
+  readonly?: boolean;
+}>();
 
 const kindOfFabricStore = useKindOfFabricStore();
 
 const filter = ref<Array<KindOfFabric>>([]);
 
-const computedOptions = computed({
-  get: () => (filter.value.length > 0 ? filter.value : kindOfFabricStore.kindOfFabrics),
-  set: (val) => (filter.value = val),
-});
+const options = ref<KindOfFabric[]>([]);
 
 function filterFn(value: string, update: (callback: () => void) => void) {
+  if (value == '') {
+    update(() => {
+      options.value = kindOfFabricStore.kindOfFabrics;
+    });
+    return;
+  }
   update(() => {
-    computedOptions.value = kindOfFabricStore.filterKindOfFabricByName(value);
+    options.value = kindOfFabricStore.kindOfFabrics.filter((k) =>
+      k.name.toLocaleLowerCase().includes(value.toLocaleLowerCase()),
+    );
   });
 }
 </script>
