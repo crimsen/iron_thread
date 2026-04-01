c75ecf4a460090296401913c83a04f08691e6378
Tim Gröger <tim@groeger-clan.de>
2026-04-01 12:24:53 +0200

    add DTO for project

--- src-tauri/src/dtos/mod.rs
+++ src-tauri/src/dtos/mod.rs
@@ -1,2 +1,3 @@
 pub mod fabric;
 pub mod fabric_x_project;
+pub mod project;
---
+++ src-tauri/src/dtos/project.rs
@@ -0,0 +1,26 @@
+use sea_orm::ActiveValue::{NotSet, Set};
+use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, NoneAsEmptyString, PickFirst};
+
+use crate::entities::project::ActiveModel;
+
+#[serde_as]
+#[derive(Debug, Clone, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ProjectDTO {
+    pub id: i32,
+    pub name: String,
+    #[serde_as(as = "PickFirst<(_, NoneAsEmptyString)>")]
+    #[serde(default)]
+    pub size: Option<i32>,
+}
+
+impl From<ProjectDTO> for ActiveModel {
+    fn from(value: ProjectDTO) -> Self {
+        Self {
+            id: if value.id < 0 { NotSet } else { Set(value.id) },
+            name: Set(value.name),
+            size: Set(value.size),
+        }
+    }
+}
