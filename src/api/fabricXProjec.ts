a9db232310c6e071dbd3077584c01931903dfa40
Tim Gröger <tim@groeger-clan.de>
2026-04-04 18:49:27 +0200

    feat(fabric): Introduce FabricSelect component and save API method
    Introduce `FabricSelect` component for selecting fabrics with filtering
    capabilities, leveraging `useFabricStore`.
    Add `save` method to `fabricXProjectApi` for persisting `FabricXProject`
    instances.
    Update return type for `getAll` in `fabricXProjectApi`.

--- src/api/fabricXProjec.ts
+++ src/api/fabricXProjec.ts
@@ -2,9 +2,11 @@ import { invoke } from '@tauri-apps/api/core';
 import type { FabricXProject } from 'src/types/fabricXProject';
 export const fabricXProjectApi = {
   async getAll(): Promise<FabricXProject[]> {
-    return await invoke<Array<FabricXProject>>('get_fabric_x_projects');
+    return await invoke<FabricXProject[]>('get_fabric_x_projects');
   },
-  // async save(fabricXProject: FabricXProject): Promise<FabricXProject> {
-  //   return Promise({})
-  // }
+  async save(fabricXProject: FabricXProject): Promise<FabricXProject> {
+    return await invoke<FabricXProject>('save_fabric_x_project', {
+      fabricXProjectData: fabricXProject,
+    });
+  },
 };
---
+++ src/components/fabrics/FabricSelect.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-select
+    v-model="model"
+    option-label="name"
+    option-value="id"
+    :options="options"
+    use-input
+    :readonly="readonly"
+    label="Fabric"
+    map-options
+    emit-value
+    @filter="filterFn"
+    input-debounce="0"
+  ></q-select>
+</template>
+
+<script setup lang="ts">
+import { useFabricStore } from 'src/stores/fabricStore';
+import { computed, onBeforeMount, ref } from 'vue';
+
+onBeforeMount(async () => {
+  await fabricStore.loadFabrics();
+});
+const filterText = ref('');
+
+const options = computed(() => {
+  const all = fabricStore.fabrics;
+  if (!filterText.value) return all;
+
+  const needle = filterText.value.toLowerCase();
+  return all.filter((v) => v.name?.toLowerCase().includes(needle));
+});
+
+function filterFn(value: string, update: (callback: () => void) => void) {
+  update(() => {
+    filterText.value = value;
+  });
+}
+const model = defineModel<number | null>();
+defineProps<{
+  readonly?: boolean;
+}>();
+
+const fabricStore = useFabricStore();
+</script>
