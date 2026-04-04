f089d86d1d734260ae87372a182dedb4ccefbe41
Tim Gröger <tim@groeger-clan.de>
2026-04-04 18:18:46 +0200

    add scripts to modify database

---
+++ pull_database_from_adb.sh
@@ -0,0 +1,2 @@
+adb exec-out "run-as de.iron_thread.app cat iron_thread.db" ./iron_thread.db
+
---
+++ push_database_to_adb.sh
@@ -0,0 +1,1 @@
+cat ./iron_thread.db | adb shell "run-as de.iron_thread.app sh -c 'cat > iron_thread.db'"
